c917547dd331e0bffa2bd2125e460ebe3edfade1
MOHAMMED EL FATIH SAIDI <elfatihsaidi@gmail.com>
2025-09-06 15:30:27 +0100

    regler Pb de dataset

--- dataset.py
+++ dataset.py
@@ -1,22 +1,12 @@
-# dataset.py
-
 import pandas as pd
 import torch
 from torch.utils.data import Dataset
 from transformers import AutoTokenizer
 
 class ArabicPlagiarismCSVDataset(Dataset):
-    """
-    Lit un CSV train/val/test et renvoie pour chaque item :
-      - s_ids, s_mask : tenseurs BERT pour le fragment suspect
-      - r_ids, r_mask : tenseurs BERT pour le fragment source
-      - label         : tenseur float (0.0 ou 1.0)
-    """
     def __init__(self, csv_path: str, max_len: int = 128):
         self.df = pd.read_csv(csv_path)
-        self.tokenizer = AutoTokenizer.from_pretrained(
-            "aubmindlab/bert-base-arabertv2"
-        )
+        self.tokenizer = AutoTokenizer.from_pretrained("aubmindlab/bert-base-arabertv2")
         self.max_len = max_len
 
     def __len__(self):
@@ -24,8 +14,6 @@ class ArabicPlagiarismCSVDataset(Dataset):
 
     def __getitem__(self, idx: int):
         row = self.df.iloc[idx]
-
-        # Tokenisation du fragment suspect
         se = self.tokenizer(
             row["suspicious_text"],
             truncation=True,
@@ -33,8 +21,6 @@ class ArabicPlagiarismCSVDataset(Dataset):
             max_length=self.max_len,
             return_tensors="pt"
         )
-
-        # Tokenisation du fragment source
         re = self.tokenizer(
             row["source_text"],
             truncation=True,
@@ -42,7 +28,6 @@ class ArabicPlagiarismCSVDataset(Dataset):
             max_length=self.max_len,
             return_tensors="pt"
         )
-
         return {
             "s_ids":  se["input_ids"].squeeze(0),
             "s_mask": se["attention_mask"].squeeze(0),
